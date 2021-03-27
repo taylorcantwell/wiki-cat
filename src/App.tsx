0894ac2280020e7c87b32bc462e4532112e7276c
Taylor Cantwell <taylorcantwell@outlook.com>
2021-03-27 13:14:59 +1100

    add footer draft

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,10 @@ import styled from 'styled-components';
 import Hero from './components/Hero';
 import MostSearched from './components/MostSearched';
 import Info from './components/Info';
+import Footer from './components/Footer';
 
 const AppShell = styled.div`
-    padding: 18px;
+    padding: 0 18px;
     margin: 0 auto;
     max-width: 1266px;
 `;
@@ -16,6 +17,7 @@ function App() {
             <Hero />
             <MostSearched />
             <Info />
+            <Footer />
         </AppShell>
     );
 }
